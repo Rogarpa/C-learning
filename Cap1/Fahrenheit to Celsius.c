13b90c145943864e8a4af44c4f36bcfda943fcb4
Rodriginsky-lap <rodlap@gmail.com>
2023-08-03 00:35:13 -0500

    Advance in chapter         mod Fahrenheit to Celsius.c             Improvement with float variables

--- Cap1/Fahrenheit to Celsius.c
+++ Cap1/Fahrenheit to Celsius.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
     /* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300 */
+    for fahr = 0, 20, ..., 300; floating-point version */
     main()
     {
-        int fahr, celsius;
-        int lower, upper, step;
+        float fahr, celsius;
+        float lower, upper, step;
         lower = 0;
         upper = 300;
         step = 20;
-        /* lower limit of temperature scale */
+        /* lower limit of temperatuire scale */
         /* upper limit */
         /* step size */
         fahr = lower;
         while (fahr <= upper) {
-            celsius = 5 * (fahr-32) / 9;
-            printf("%d\t%d\n", fahr, celsius);
+            celsius = (5.0/9.0) * (fahr-32.0);
+            printf("%3.0f %6.1f\n", fahr, celsius);
             fahr = fahr + step;
         }
     }
