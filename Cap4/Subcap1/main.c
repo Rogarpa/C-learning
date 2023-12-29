5947fe8e8ff51ba29bdc8c013b5812e366229b8a
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-12-29 17:52:36 -0600

    Added strindex modification (4.01)             mod Cap4/Subcap1/main.c                 calling to strindexrightmost instead of strindex             mod Cap4/Subcap1/strindex.c                 added excercise strindexrightmost function

--- Cap4/Subcap1/main.c
+++ Cap4/Subcap1/main.c
@@ -5,15 +5,18 @@
 
 int getLine(char line[], int max);
 int strindex(char source[], char searchfor[]);
+int strindexrightmost(char source[], char searchfor[]);
 char pattern[] = "ould";   /* pattern to search for */
 /* find all lines matching pattern */
 int main()
 {
     char line[MAXLINE];
     int found = 0;
+    int indexfound = 0;
     while (getLine(line, MAXLINE) > 0)
-        if (strindex(line, pattern) >= 0) {
+        if ((indexfound = strindexrightmost(line, pattern)) >= 0) {
             printf("%s", line);
+            printf(", index found = %d", indexfound);
             found++;
         }
     return found;
--- Cap4/Subcap1/strindex.c
+++ Cap4/Subcap1/strindex.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
-   /* strindex:  return index of t in s, -1 if none */
-   int strindex(char s[], char t[])
-   {
-       int i, j, k;
-       for (i = 0; s[i] != '\0'; i++) {
-           for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
-               ;
-           if (k > 0 && t[k] == '\0')
-               return i;
-       }
-       return -1;
-   }
+    /* strindex:  return index of t in s, -1 if none */
+    int strindex(char s[], char t[])
+    {
+        int i, j, k;
+        for (i = 0; s[i] != '\0'; i++) {
+            for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+                ;
+            if (k > 0 && t[k] == '\0')
+                return i;
+        }
+        return -1;
+    }
+    /* strindex:  return index of rightmost ocurrence of t in s, -1 if none */
+    int strindexrightmost(char s[], char t[])
+    {
+        int i, j, k;
+        int rightmost = -1;
+        for (i = 0; s[i] != '\0'; i++) {
+            for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+                ;
+            if (k > 0 && t[k] == '\0')
+                rightmost =  i;
+        }
+        
+        return rightmost;
+    }
