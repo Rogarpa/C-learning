9f4b2e9d528b2ae6e129493b4781bca87f9af6b2
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-12-29 17:52:36 -0600

    Added Longer Line program             add Cap1/LongerLine.c                 Longer Line program

---
+++ Cap1/LongerLines.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+    #define MAXLINE 1000   /* maximum input line length */
+    #define MAXLINES 1000   /* maximum input lines buffered */
+    int getLine(char line[], int maxline);
+    void copy(char to[], char from[]);
+    /* print the longest input line */
+    main()
+    {
+        int longerLineIndex;
+        int len;            /* current line length */
+        int max;            /* maximum length seen so far */
+        char line[MAXLINE];    /* current input line */
+        char longerLine[MAXLINES][MAXLINE]; /* longest line saved here */
+        longerLineIndex = 0;
+        while ((len = getLine(line, MAXLINE)) > 0){
+            if (len > 80 && longerLineIndex  < MAXLINES) {
+                copy(longerLine[longerLineIndex], line);
+                ++longerLineIndex;
+            }
+        }
+        
+
+        for(longerLineIndex = 0; longerLineIndex <  MAXLINES; ++longerLineIndex){
+            printf(longerLine[longerLineIndex]);
+        }
+        
+        return 0;
+    }
+    /* getLine:  read a line into s, return length  */
+    int getLine(char s[],int lim)
+    {
+        int c, i;
+        for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+            s[i] = c;
+        if (c == '\n') {
+            s[i] = c;
+            ++i;
+        }
+        s[i] = '\0';
+        return i;
+    }
+
+    /* copy:  copy 'from' into 'to'; assume to is big enough */
+    void copy(char to[], char from[])
+    {
+        int i;
+        i = 0;
+        while ((to[i] = from[i]) != '\0')
+            ++i;
+    }
