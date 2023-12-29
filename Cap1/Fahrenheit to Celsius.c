8c9432cf03ccb3f7c9e6208c2366e9525585eae2
Rodriginsky-lap <rodlap@gmail.com>
2023-12-28 19:28:41 -0600

    Advance in chapter                 mod Fahrenheit to Celsius.c                     With symbolic constants

--- Cap1/Fahrenheit to Celsius.c
+++ Cap1/Fahrenheit to Celsius.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-    /* print Fahrenheit-Celsius table */
-    main()
-    {
-        int fahr;
-        for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+#define LOWER  0     /* lower limit of table */
+#define UPPER  300   /* upper limit */
+#define STEP   20    /* step size */
+/* print Fahrenheit-Celsius table */
+int main()
+{
+    int fahr;
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-    }
+}
