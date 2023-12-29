4f72fa7c7acf0062fc67d06931317bb07c3b3da7
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-12-29 17:52:36 -0600

    Added variables to Polish Calculator (4.06)             mod Cap4/Subcap3/polishCalculator.c                 Added one char variables

--- Cap4/Subcap3/polishCalculator.c
+++ Cap4/Subcap3/polishCalculator.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>  /* for  atof() */
 #include <math.h>  /* for  sin, exp and pow */
+#include <ctype.h>
 #define MAXOP   100  /* max size of operand or operator */
 #define NUMBER  '0'  /* signal that a number was found */
+#define ALPHABET_SIZE  26  /* maximum depth of val stack */
+double memmory[ALPHABET_SIZE]; /*for calculator variables*/
 int getop(char []);
 void push(double);
 double pop(void);
+double peek(void);
 /* reverse Polish calculator */
+
 int main()
 {
     int type;
@@ -49,9 +54,15 @@ int main()
                 push(pow(pop(), op2));
                 break;
             case '\n':
+                memmory['l' - 'a'] = peek();
                 printf("\t%.8g\n", pop());
                 break;
             default:
+                if(isalpha(type) && islower(type)){
+                    push(memmory[type-'a']);
+                    break;
+                }
+
                 printf("error: unknown command %s\n", s);
                 break;
         }
