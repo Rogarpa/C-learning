e6394935800e57672046769278fb65473f11027e
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-12-29 17:52:36 -0600

    Refactoring of excercise             mod Fahrenheit to Celsius.c                 Improvement with conversion function

--- Cap1/Fahrenheit to Celsius.c
+++ Cap1/Fahrenheit to Celsius.c
@@ -3,9 +3,14 @@
 #define UPPER  300   /* upper limit */
 #define STEP   20    /* step size */
 /* print Fahrenheit-Celsius table */
+double convertFahrToCelsius(int fahr);
 int main()
 {
     int fahr;
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        printf("%3d %6.1f\n", fahr, convertFahrToCelsius(fahr));
+}
+
+double convertFahrToCelsius(int fahr){
+    return (double)((5.0/9.0)*(fahr-32));
 }
