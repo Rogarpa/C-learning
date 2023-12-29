c55d7d3cc3e337303ba290a9e054084ad749474c
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-12-29 17:52:36 -0600

    Added operations to stack data structure excercise (4.04)             mod Cap4/Subcap3/stack.c                 Added peeking, printingPeek, duplicate, swap and clear                 operations

--- Cap4/Subcap3/stack.c
+++ Cap4/Subcap3/stack.c
@@ -20,3 +20,43 @@ double pop(void)
         return 0.0;
     }
 }
+double peek(void)
+{
+    if (sp > 0)
+        return val[sp-1];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+void printPeek(void)
+{
+    if (sp > 0)
+        printf("%f", val[sp-1]);
+    else {
+        printf("error: stack empty\n");
+    }
+}
+void duplicate(void)
+{
+    if (sp < MAXVAL)
+        val[sp++] = peek();
+    else
+        printf("error: stack full, can't duplicate\n");
+}
+void swap(void)
+{
+    if(sp > 1){
+        printf("error: stack size is not enough\n");
+    }
+
+    double pop1, pop2;
+    pop1 = pop();
+    pop2 = pop();
+    push(pop1);
+    push(pop2);
+}
+void clear(void)
+{
+    int sp = 0;
+}
