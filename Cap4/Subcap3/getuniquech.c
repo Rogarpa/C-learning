ca43ae500d1ac37a769761f285342f78547c6a04
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-12-29 17:52:36 -0600

    Added handling for EOF in ungetch (4.09)             mod Cap4/Subcap3/getch.c                 Added condition for getch and ungetch EOF char

--- Cap4/Subcap3/getuniquech.c
+++ Cap4/Subcap3/getuniquech.c
@@ -5,6 +5,10 @@ int availableBuffer = 0;
 int getuniquech(void)  /* get a (possibly pushed-back) character */
 {
     if(availableBuffer > 0){
+        if((buf == EOF) && (getchar() != EOF)){
+            printf("getuniquech: getting EOF while input stream not ended\n");
+            return;
+        }
         availableBuffer = 0;
         return buf;
     }
@@ -13,8 +17,13 @@ int getuniquech(void)  /* get a (possibly pushed-back) character */
 void ungetuniquech(int c)   /* push character back on input */
 {
     if (availableBuffer > 0)
-        printf("ungetch: too many characters\n");
+        printf("ungetuniquech: too many characters\n");
     else{
+        if((c == EOF) && ((buf = getchar()) != EOF)){
+            availableBuffer = 1;
+            printf("ungetuniquech: ungetting EOF while input stream not ended\n");
+            return;
+        }
         buf = c;
         availableBuffer = 1;
     }
